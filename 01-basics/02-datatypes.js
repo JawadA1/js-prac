b8468c0f8ccf7a370510a27d15794819a34f3bfb
JawadA1 <mjadu1112@gmail.com>
2024-05-23 11:20:54 +0000

    datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,23 @@
+//  alert( 3 + 3 ) //  alert is used in browser and we are using nide js
+
+console.log(3 
+    +3)   //  this can be run but its not readable code readibility should be high
+
+
+
+/*
+number => it stores numeric values
+bigint =>it stores large numeric values
+strins => "" you can also use single qoutes
+boolean => true/false it shows that value is either true or false
+null => standalone value
+undifined => its the detatype when var is not declared
+symbol => it is used for uniqueness
+
+*/
+
+
+
+// typeof is used to check the datatype of variable
+console.log(typeof null); // but if we check the datatype of null it is object 
+console.log(typeof undefined); // and dataype of undifined is undifined 
